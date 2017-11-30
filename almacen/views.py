b8314353324ce32b8cc57f691f653b6dd2976794
Oscar <oscar_mendoza@upeu.edu.pe>
2017-11-30 10:57:54 -0500

    first commit

--- almacen/views.py
+++ almacen/views.py
@@ -8,13 +8,8 @@ from rest_framework.serializers import ModelSerializer
 from rest_framework.response import Response
 
 
-class CategoriaSerializer(ModelSerializer):
-    class Meta:
-        model = Categoria
-        fields = '__all__'
 
 class ProductoSerializer(ModelSerializer):
-    categoria = CategoriaSerializer()
     class Meta:
         model = Producto
         fields = '__all__'
@@ -24,12 +19,3 @@ class ProductoViewSet(viewsets.ModelViewSet):
     queryset = Producto.objects.all()
     serializer_class = ProductoSerializer
 
-    def list(self, request):
-        try:
-            parent_id = request.query_params['parent']
-            queryset = Producto.objects.filter(categoria_id=parent_id)
-        except MultiValueDictKeyError:
-            queryset = Producto.objects.all()
-
-        serializer = ProductoSerializer(queryset, many=True)
-        return Response(serializer.data)
